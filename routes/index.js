c7edd184f562761b15155fe32765b92961b2b175
Abdullahi Muritala <oabdullahi37@gmail.com>
2022-10-14 02:12:24 +0100

    Add new routes to capture and store new messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
+// messages
+
+const messages = [
+  {
+    text: "It's cold at the top",
+    user: 'Bello',
+    added: new Date(),
+  },
+  {
+    text: 'Que sera sera',
+    user: 'Unknown',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Mini MessageBoard', messages: messages });
+});
+
+router.get('/new', function (req, res, next) {
+  res.render('form', { title: 'Add New Message To The Board' });
+});
+
+router.post('/new', function (req, res, next) {
+  const { messageText, messageUser } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
